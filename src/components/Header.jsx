d9c9406aceb0ac278014e10a5105b60a533aafc8
Daivya Singh <daivya@hotmail.com>
2024-06-30 00:54:23 -0400

    Updates

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,25 @@
+import React, { useState } from 'react';
 import './Header.css';
 import Spacer from './spacer.jsx';
-
+import Menu from "./Menu.jsx";
+import Padder from "./padder.jsx";
 
 function Header() {
+    const [isOpen, setIsOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setIsOpen(!isOpen);
+    };
+
     return (
-        <header>
-            <img src="src/assets/CSHS-Logo.png" id="headerLogo" alt="logo"/>
-            <h1 id="title">My Blog</h1>
-            <Spacer />
+        <header className={`header ${isOpen ? 'open' : ''}`}>
+            <div className="minorBlock">
+                <img src="src/assets/CSHS-Logo.png" id="headerLogo" alt="logo"/>
+                <h1 id="title">My Blog</h1>
+                <Spacer />
+                <Menu className = "menu" isOpen={isOpen} toggleMenu={() => {toggleMenu(); console.log(isOpen);}} />
+                <Padder />
+            </div>
         </header>
     );
 }
--- src/components/HighlightButton.jsx
+++ src/components/HighlightButton.jsx
@@ -1,12 +1,33 @@
 import './HighlightButton.css'
 
+function randomRotate(event) {
+    var random = Math.random()*30 - 10;
+
+    while (random > -5 && random < 5) {
+        random = Math.random()*20 - 10;
+    }
+
+    event.target.style.setProperty('transform', 'scale(0.9) rotate(' + random + 'deg)');
+}
+
+function resetTransform(event) {
+    event.target.style.setProperty('transform', '');
+}
+
 // eslint-disable-next-line react/prop-types
 function HighlightButton({ text }) {
-  return (
-      <div>
-        <button className="highlight"> {text} </button>
-      </div>
-  );
+    return (
+        <div>
+            <button className="highlight"
+                    onMouseOver={randomRotate}
+                    //onMouseDown={randomRotate}
+                    onMouseOut={resetTransform}
+                    //onMouseUp={resetTransform}
+                >
+                {text}
+            </button>
+        </div>
+    );
 }
 
 export default HighlightButton;
