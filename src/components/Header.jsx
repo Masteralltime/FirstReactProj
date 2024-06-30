ac2b54cb795f9dea45039d21909bfe1fa07a8e0d
Daivya Singh <daivya@hotmail.com>
2024-06-30 01:45:01 -0400

    Updates

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -14,8 +14,9 @@ function Header() {
     return (
         <header className={`header ${isOpen ? 'open' : ''}`}>
             <div className="minorBlock">
+                <Padder />
                 <img src="src/assets/CSHS-Logo.png" id="headerLogo" alt="logo"/>
-                <h1 id="title">My Blog</h1>
+                <h1 id="title">RaptorHub</h1>
                 <Spacer />
                 <Menu className = "menu" isOpen={isOpen} toggleMenu={() => {toggleMenu(); console.log(isOpen);}} />
                 <Padder />
---
+++ src/components/SearchBar.jsx
@@ -0,0 +1,11 @@
+import './SearchBar.css';
+
+function SearchBar() {
+    return (
+        <div className="searchBar">
+            <input type="text" id="searchBar" placeholder="Search"/>
+        </div>
+    );
+}
+
+export default SearchBar;
