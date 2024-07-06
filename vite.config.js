5755e1983116f541e5033ec206817b1909090649
Daivya Singh <daivya@hotmail.com>
2024-07-06 15:05:57 -0400

    New page

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:"/FirstReactProj/",
+  base:"https://masteralltime.github.io/FirstReactProj",
   plugins: [react()],
 })
